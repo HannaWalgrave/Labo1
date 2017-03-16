162435a5c7a98dcd25d78235f13817a77279d74d
ToastedSalad <Walgrave.hanna@hotmail.com>
2017-03-16 18:19:17 +0100

    finished excersice ES6 workshop

--- Week3/ES6-workshop/js/app.es5.js
+++ Week3/ES6-workshop/js/app.es5.js
@@ -1,39 +1,59 @@
 "use strict";
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 var firstname = "hanna";
 console.log("hi my name is " + firstname);
 
-function CardApp() {
-    this.buttonAddNote = document.getElementById("btnAddNote");
-    this.notesContainer = document.querySelector(".notes");
-    this.noteInput = document.querySelector("#txtAddNote");
-    this.buttonAddNote.addEventListener("click", this.addNote.bind(this));
-}
-CardApp.prototype.resetForm = function () {
-    this.noteInput.value = "";
-    this.noteInput.focus();
-};
-CardApp.prototype.addNote = function (e) {
-    var newNote = document.createElement("div");
-    newNote.setAttribute("class", "card");
-    newNote.innerHTML = "<p>" + this.noteInput.value + "</p>";
+var CardApp = function () {
+    function CardApp() {
+        _classCallCheck(this, CardApp);
 
-    var noteLink = document.createElement("a");
-    noteLink.setAttribute("class", "card-remove");
-    noteLink.innerHTML = "Remove";
-    noteLink.setAttribute("href", "#");
-    noteLink.addEventListener("click", this.removeNote.bind(this));
+        this.buttonAddNote = document.getElementById("btnAddNote");
+        this.notesContainer = document.querySelector(".notes");
+        this.noteInput = document.querySelector("#txtAddNote");
+        this.buttonAddNote.addEventListener("click", this.addNote.bind(this));
+    }
 
-    newNote.appendChild(noteLink);
+    _createClass(CardApp, [{
+        key: "addNote",
+        value: function addNote(e) {
 
-    this.notesContainer.appendChild(newNote);
-    this.resetForm();
-};
-CardApp.prototype.removeNote = function (e) {
-    var noteToRemove = e.target.parentElement;
-    this.notesContainer.removeChild(noteToRemove);
-    e.preventDefault();
-};
+            var newNote = document.createElement("div");
+            newNote.setAttribute("class", "card");
+            newNote.innerHTML = "<p>" + this.noteInput.value + "</p>";
+            var noteLink = document.createElement("a");
+            noteLink.setAttribute("class", "card-remove");
+            noteLink.innerHTML = "Remove";
+            noteLink.setAttribute("href", "#");
+            noteLink.addEventListener("click", this.removeNote.bind(this));
+
+            newNote.appendChild(noteLink);
+
+            this.notesContainer.appendChild(newNote);
+            this.resetForm();
+
+            e.preventDefault();
+        }
+    }, {
+        key: "removeNote",
+        value: function removeNote(event) {
+            var noteToRemove = event.target.parentElement;
+            this.notesContainer.removeChild(noteToRemove);
+            event.preventDefault();
+        }
+    }, {
+        key: "resetForm",
+        value: function resetForm() {
+            this.noteInput.value = "";
+            this.noteInput.focus();
+        }
+    }]);
+
+    return CardApp;
+}();
 
 var myApp = new CardApp();
 
--- Week3/ES6-workshop/js/app.es6.js
+++ Week3/ES6-workshop/js/app.es6.js
@@ -1,21 +1,21 @@
 let firstname = "hanna";
 console.log(`hi my name is ${firstname}`);
 
-function CardApp(){
-    this.buttonAddNote = document.getElementById("btnAddNote");
+class CardApp{
+    
+    constructor()
+    {
+        this.buttonAddNote = document.getElementById("btnAddNote");
     this.notesContainer = document.querySelector(".notes");
     this.noteInput = document.querySelector("#txtAddNote");
     this.buttonAddNote.addEventListener("click", this.addNote.bind(this));
-}
-CardApp.prototype.resetForm = function(){
-    this.noteInput.value ="";
-    this.noteInput.focus();
-}
-CardApp.prototype.addNote = function(e){
+    }
+
+    addNote(e){
+        
     let newNote = document.createElement("div");
     newNote.setAttribute("class", "card");
     newNote.innerHTML = "<p>" + this.noteInput.value +"</p>";
-    
     let noteLink = document.createElement("a");
     noteLink.setAttribute("class", "card-remove");
     noteLink.innerHTML = "Remove";
@@ -26,13 +26,21 @@ CardApp.prototype.addNote = function(e){
     
     this.notesContainer.appendChild(newNote);
     this.resetForm();
-
-}
-CardApp.prototype.removeNote = function(e){
-    let noteToRemove = e.target.parentElement;
-    this.notesContainer.removeChild(noteToRemove);
+        
     e.preventDefault();
+        
+    }
+    
+    removeNote(event){
+        let noteToRemove = event.target.parentElement;
+        this.notesContainer.removeChild(noteToRemove);
+        event.preventDefault();
+    }
+    
+        resetForm(){
+        this.noteInput.value ="";
+        this.noteInput.focus();
+    }
+
 }
-
-
 let myApp = new CardApp();
