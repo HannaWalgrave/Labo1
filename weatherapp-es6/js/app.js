0924b658fb53cf1246f0deb336d674b12653b7f4
Hanna Walgrave <Walgrave.hanna@hotmail.com>
2017-05-06 10:21:06 +0200

    added break lines in <p>

--- weatherapp-es6/js/app.js
+++ weatherapp-es6/js/app.js
@@ -58,8 +58,8 @@ class Weather
         if (sum.includes("Cloudy") == true){
             color = "#D9D9D9";
             icon = "/images/iconCloudy.svg" ;
-            expl = "Voorzie genoeg water voor je kiemende worteltjes"+
-                " Stilstand van groei door vochtgebrek moet vermeden worden, daar dit zorgt voor voze of gescheurde worteltjes."
+            expl = "Voorzie genoeg water voor je kiemende worteltjes"+ "<br>" +
+                " Stilstand van groei door vochtgebrek moet vermeden worden, daar dit zorgt voor voze of gescheurde worteltjes.";
         }
         else if (sum.includes("Clear") == true){
             color = "#87CEEB";
@@ -67,14 +67,14 @@ class Weather
             expl = "Voorzie genoeg water voor je kiemende worteltjes"+
                 " een droge bovengrond wil ook niet altijd zeggen dat je plant te weinig water heeft"+
                 " Graaf een klein stukje in de grond om te zien of de grond nog vochtig is"+
-                " Stilstand van groei door vochtgebrek moet vermeden worden, daar dit zorgt voor voze of gescheurde worteltjes."
+                " Stilstand van groei door vochtgebrek moet vermeden worden, daar dit zorgt voor voze of gescheurde worteltjes.";
         }
         else if (sum.includes("Rain") == true){
             color = "#BDBDBD";
             expl = "Let op dat je wortelen niet te nat staan!" +
                 "Dek je perceel of potten bijvoorbeeld af met stro of een dikke laag mulch."+
                 "Een te hoge schommeling in de vochtigheid van de bodem kan ervoor zorgen dat de wortel barst."+
-                "Probeer te wieden tijdens regendagen en bij veel wind, dan is de wortelvlieg veel minder actief. "
+                "Probeer te wieden tijdens regendagen en bij veel wind, dan is de wortelvlieg veel minder actief. ";
         }
         $("body").css("background-color",color);
         $("#app").append(`<h1>${Math.round(this.weather.temperature)}&deg;</h1>`);
