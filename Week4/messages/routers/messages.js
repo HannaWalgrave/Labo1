84f1c8dd8a62516959140cd9db729b2cde2b11c7
Hanna Walgrave <Walgrave.hanna@hotmail.com>
2017-05-13 10:41:07 +0200

    added routers

---
+++ Week4/messages/routers/messages.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    res.send("GET messages");
+});
+
+router.get('/:id', function (req, res) {
+    var id = req.params.id;
+    res.send("GET message with :id " + id);
+});
+
+router.post('/', function (req, res){
+    res.send("POST messages");
+});
+
+module.exports = router;
